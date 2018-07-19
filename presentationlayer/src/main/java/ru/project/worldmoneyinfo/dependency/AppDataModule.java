f718008f7786a15fd58886efa7e41d1657b96e32
Titan-mw <myprbus@gmail.com>
2018-07-19 11:37:23 +0200

    Refactoring performed.

--- presentationlayer/src/main/java/ru/project/worldmoneyinfo/dependency/AppDataModule.java
+++ presentationlayer/src/main/java/ru/project/worldmoneyinfo/dependency/AppDataModule.java
@@ -1,32 +1,18 @@
 package ru.project.worldmoneyinfo.dependency;
 
-import javax.inject.Named;
-
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppDataModule {
-    public static final String PAIRS_KEY = "Pairs";
-    public static final String API_KEY = "ApiKey";
-    private String mCurrenciesPairs;
     private String mApiKey;
 
-    public AppDataModule(String pairs, String apiKey) {
-        mCurrenciesPairs = pairs;
+    public AppDataModule(String apiKey) {
         mApiKey = apiKey;
     }
 
     @Provides
     @IScreenScope
-    @Named(PAIRS_KEY)
-    public String providePairs() {
-        return mCurrenciesPairs;
-    }
-
-    @Provides
-    @IScreenScope
-    @Named(API_KEY)
     public String provideApiKey() {
         return mApiKey;
     }
--- presentationlayer/src/main/java/ru/project/worldmoneyinfo/ui/currencies_list_screen/CurrenciesListFragment.java
+++ presentationlayer/src/main/java/ru/project/worldmoneyinfo/ui/currencies_list_screen/CurrenciesListFragment.java
@@ -35,7 +35,7 @@ public class CurrenciesListFragment extends BaseFragment {
                 new RepositoryModule(),
                 new ServiceModule(),
                 // use your own key for result
-                new AppDataModule("", "")
+                new AppDataModule("")
         ).inject(this);
     }
 
