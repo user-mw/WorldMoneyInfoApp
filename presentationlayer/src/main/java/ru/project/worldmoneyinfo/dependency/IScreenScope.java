1a0e68edf57d7538846f54e1ab2423fb74478235
Titan-mw <myprbus@gmail.com>
2018-07-22 19:10:48 +0200

    Code updated.

--- presentationlayer/src/main/java/ru/project/worldmoneyinfo/dependency/IScreenScope.java
+++ presentationlayer/src/main/java/ru/project/worldmoneyinfo/dependency/IScreenScope.java
@@ -1,7 +1,11 @@
 package ru.project.worldmoneyinfo.dependency;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import javax.inject.Scope;
 
 @Scope
+@Retention(RetentionPolicy.RUNTIME)
 public @interface IScreenScope {
 }
